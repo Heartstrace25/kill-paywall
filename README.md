26f6a8a5ed904dbfe69af056cc52d2c0df6be122
Heartstrace25 <58144465+Heartstrace25@users.noreply.github.com>
2019-11-24 10:52:38 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,149 @@
-# kill-paywall
-I dont take credit for this, im  just starting to learn java, and was trying to edit a pre-made extenstion to better fit my needs. it's not working when i try to plug it into chrome  ( i only added one line of code) and i wasnt sure if there was something ekse in the code pointing back to a github project for update purposes, so im uploading my edited extension to see if that will satisfy that inquiry, if that IS a thing in the original code.
+# Bypass Paywalls for Chrome
+
+### Installation Instructions
+**Google Chrome**
+1. Download this repo as a [ZIP file from GitHub](https://github.com/iamadamdev/bypass-paywalls-chrome/archive/master.zip).
+1. Unzip the file and you should have a folder named `bypass-paywalls-chrome-master`.
+1. In Chrome go to the extensions page (`chrome://extensions`).
+1. Enable Developer Mode.
+1. Drag the `bypass-paywalls-chrome-master` folder anywhere on the page to import it.
+
+**Notes**
+* Every time you open Chrome it may warn you about running extensions in developer mode, just click 🗙 to keep the extension enabled.
+* You will be logged out for any site you have checked.
+* This extension works best along side the adblocker uBlock Origin.
+
+### Bypass the following sites' paywalls with this extension
+
+[Algemeen Dagblad](https://www.ad.nl)\
+[American Banker](https://americanbanker.com)\
+[Baltimore Sun](https://www.baltimoresun.com)\
+[Barron's](https://www.barrons.com)\
+[Bloomberg](https://www.bloomberg.com)\
+[Bloomberg Quint](https://bloombergquint.com)\
+[Business Insider](https://www.businessinsider.com)\
+[Chicago Tribune](https://www.chicagotribune.com)\
+[Corriere Della Sera](https://www.corriere.it)\
+[Crain's Chicago Business](https://www.chicagobusiness.com)\
+[Dagens Nyheter](https://dn.se)\
+[Daily Press](https://www.dailypress.com)\
+[De Groene Amsterdammer](https://www.groene.nl)\
+[De Volkskrant](https://www.volkskrant.nl)\
+[DeMorgen](https://demorgen.be)\
+[Denver Post](https://www.denverpost.com)\
+[Die Zeit](https://www.zeit.de)\
+[Dynamed Plus](https://dynamed.com)\
+[Eindhovens Dagblad](https://ed.nl)\
+[Encyclopedia Britannica](https://britannica.com)\
+[Examiner](https://examiner.com.au)\
+[First Things](https://www.firstthings.com)\
+[Financial News](https://fnlondon.com)\
+[Financial Times](https://www.ft.com)\
+[Foreign Policy](https://www.foreignpolicy.com)\
+[Glassdoor](https://www.glassdoor.com)\
+[Haaretz.co.il](https://www.haaretz.co.il)\
+[Haaretz.com](https://www.haaretz.com)\
+[Harper's Magazine](https://harpers.org)\
+[Hartford Courant](https://www.courant.com)\
+[Harvard Business Review](https://www.hbr.org)\
+[Inc.com](https://www.inc.com)\
+[L.A. Business Journal](https://labusinessjournal.com)\
+[La Nación](https://www.lanacion.com.ar)\
+[La Repubblica](https://www.repubblica.it)\
+[La Tercera](https://latercera.com)\
+[L'Écho](https://lecho.be)\
+[Le Devoir](https://www.ledevoir.com)\
+[Le Monde](https://www.lemonde.fr)\
+[Le Parisien](http://www.leparisien.fr)\
+[Les Échos](https://lesechos.fr)\
+[Libération](https://liberation.fr)\
+[Loeb Classical Library](https://www.loebclassics.com)\
+[Los Angeles Times](https://www.latimes.com)\
+[MIT Sloan Management Review](https://sloanreview.mit.edu)\
+[MIT Technology Review](https://www.technologyreview.com)\
+[Medium](https://www.medium.com)\
+[Mexicon News Daily](https://mexiconewsdaily.com)\
+[NRC Handelsblad](https://www.nrc.nl)\
+[New York Magazine](https://www.nymag.com)\
+[New Zealand Herald](https://www.nzherald.co.nz)\
+[Newsrep](https://thenewsrep.com)\
+[Nikkei Asian Review](https://asia.nikkei.com)\
+[Orlando Sentinel](https://www.orlandosentinel.com)\
+[Parool](https://www.parool.nl)\
+[Quartz](https://qz.com)\
+[Quora](https://www.quora.com)\
+[San Diego Union Tribune](https://sandiegouniontribune.com)\
+[San Francisco Chronicle](https://sfchronicle.com)\
+[Scientific American](https://scientificamerican.com)\
+[Statista](https://statista.com)\
+[SunSentinel](https://www.sun-sentinel.com)\
+[Telegraaf](https://telegraaf.nl)\
+[The Advocate](https://www.theadvocate.com.au)\
+[The Age](https://www.theage.com.au)\
+[The American Interest](https://www.the-american-interest.com)\
+[The Atlantic](https://theatlantic.com)\
+[The Australian Financial Review](https://www.afr.com)\
+[The Australian](https://www.theaustralian.com.au)\
+[The Boston Globe](https://www.bostonglobe.com)\
+[The Business Journals](https://www.bizjournals.com)\
+[The Canberra Times](https://www.canberratimes.com.au)\
+[The Economist](https://www.economist.com)\
+[The Globe and Mail](https://www.theglobeandmail.com)\
+[The Hindu](https://www.thehindu.com)\
+[The Irish Times](https://www.irishtimes.com)\
+[The Japan Times](https://www.japantimes.co.jp)\
+[The Mercury News](https://www.mercurynews.com)\
+[The Morning Call](https://www.mcall.com)\
+[The Nation](https://www.thenation.com)\
+[The New Statesman](https://newstatesman.com)\
+[The New York Times](https://www.nytimes.com)\
+[The New Yorker](https://www.newyorker.com)\
+[The News-Gazette](https://www.news-gazette.com)\
+[The Philadelphia Inquirer](https://inquirer.com)\
+[The Seattle Times](https://www.seattletimes.com)\
+[The Spectator](https://www.spectator.co.uk)\
+[The Sydney Morning Herald](https://www.smh.com.au)\
+[The Telegraph](https://www.telegraph.co.uk)\
+[The Times](https://www.thetimes.co.uk)\
+[The Toronto Star](https://www.thestar.com)\
+[The Wall Street Journal](https://www.wsj.com)\
+[The Washington Post](https://www.washingtonpost.com)\
+[TheMarker](https://www.themarker.com)\
+[Trouw](https://www.trouw.nl)\
+[Vanity Fair](https://www.vanityfair.com)\
+[Winston-Salem Journal](https://journalnow.com)\
+[Vrij Nederland](https://vn.nl)\
+[Wired](https://www.wired.com)
+
+### Sites with limited number of free articles
+The free article limit can normally be bypassed by removing cookies for the site.*
+
+Install the [Cookie Remover](https://chrome.google.com/webstore/detail/cookie-remover/kcgpggonjhmeaejebeoeomdlohicfhce) extension.
+
+When coming across a paywall click the cookie icon to remove the cookies then refresh the page.
+
+_*May not always succeed_
+
+### New site requests
+Only large or major sites will be considered.
+1. Visit an article on the site you want to bypass the paywall for and copy the article title.
+2. Open up a new incognito window (Ctrl+Shift+N) and paste the article title into Google.
+3. Click on the same article from the Google search results page. If it loads without a paywall you can [submit a request](https://github.com/iamadamdev/bypass-paywalls-chrome/issues/new) to add the site, otherwise my extension cannot bypass it either.
+
+Usually premium articles cannot be bypassed as they are behind a hard paywall.
+
+### Troubleshooting
+* This extension works best alongside [uBlock Origin](https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm).
+* If a site doesn't work, try turning off uBlock and refreshing. Also try reinstalling the extension.
+* Make sure the site is checked under Options (on macOS sometimes the sites are unselected).
+* Make sure you're running the last version of Bypass Paywalls. 
+* If none of these work, you can submit an issue [here](https://github.com/iamadamdev/bypass-paywalls-chrome/issues).
+
+### Pull Requests
+* PRs are welcome.
+
+### Show your support
+* I am not asking for donations or anything like that, all I ask is that you star this repo.
+
+### License
+Bypass Paywalls is [MIT-licensed](https://github.com/iamadamdev/bypass-paywalls-chrome/blob/master/LICENSE).
---
+++ background.js
@@ -0,0 +1,477 @@
+/* Please respect alphabetical order when adding a site in any list */
+
+'use strict';
+
+// Cookies from this list are blocked by default
+var defaultSites = {
+  'Algemeen Dagblad': 'ad.nl', 
+  'American Banker': 'americanbanker.com',
+  'Baltimore Sun': 'baltimoresun.com',
+  'Barron\'s': 'barrons.com',
+  'Bloomberg': 'bloomberg.com',
+  'Bloomberg Quint': 'bloombergquint.com',
+  'Business Insider': 'businessinsider.com',
+  'Crain\'s Chicago Business': 'chicagobusiness.com',
+  'Chicago Tribune': 'chicagotribune.com',
+  'Corriere Della Sera': 'corriere.it',
+  'Daily Press': 'dailypress.com',
+  'Dagens Nyheter': 'dn.se',
+  'De Groene Amsterdammer': 'groene.nl',
+  'De Volkskrant': 'volkskrant.nl',
+  'DeMorgen': 'demorgen.be',
+  'Denver Post': 'denverpost.com',
+  'Die Zeit': 'zeit.de',
+  'Dynamed Plus': 'dynamed.com',
+  'Eindhovens Dagblad': 'ed.nl',
+  'Encyclopedia Britannica': 'britannica.com',
+  'Examiner': 'examiner.com.au',
+  'First Things': 'firstthings.com',
+  'Financial News': 'fnlondon.com',
+  'Financial Times': 'ft.com',
+  'Forbes': 'forbes.com',
+  'Foreign Policy': 'foreignpolicy.com',
+  'Glassdoor': 'glassdoor.com',
+  'Haaretz': 'haaretz.co.il',
+  'Haaretz English': 'haaretz.com',
+  'Harper\'s Magazine': 'harpers.org',
+  'Hartford Courant': 'courant.com',
+  'Harvard Business Review': 'hbr.org',
+  'Inc.com': 'inc.com',
+  'La Nacion': 'lanacion.com.ar',
+  'La Repubblica': 'repubblica.it',
+  'La Tercera': 'latercera.com',
+  'L\'Echo': 'lecho.be',
+  'Le Devoir': 'ledevoir.com',
+  'Le Monde': 'lemonde.fr',
+  'Le Parisien': 'leparisien.fr',
+  'Les Echos': 'lesechos.fr',
+  'Liberation': 'liberation.fr',
+  'Loeb Classical Library': 'loebclassics.com',
+  'Los Angeles Business Journal': 'labusinessjournal.com',
+  'Los Angeles Times': 'latimes.com',
+  'Medium': 'medium.com',
+  'Mexico News Daily': 'mexiconewsdaily.com',
+  'MIT Sloan Management Review': 'sloanreview.mit.edu',
+  'MIT Technology Review': 'technologyreview.com',
+  'Newsrep': 'thenewsrep.com',
+  'New York Magazine': 'nymag.com',
+  'Nikkei Asian Review': 'asia.nikkei.com',
+  'NRC': 'nrc.nl',
+  'New Zealand Herald': 'nzherald.co.nz',
+  'OrlandoSentinel': 'orlandosentinel.com',
+  'Parool': 'parool.nl',
+  'Quartz': 'qz.com',
+  'Quora': 'quora.com',
+  'San Diego Union Tribune': 'sandiegouniontribune.com',
+  'San Francisco Chronicle': 'sfchronicle.com',
+  'Scientific American': 'scientificamerican.com',
+  'Statista': 'statista.com',
+  'SunSentinel': 'sun-sentinel.com',
+  'Telegraaf': 'telegraaf.nl',
+  'The Advocate': 'theadvocate.com.au',
+  'The Age': 'theage.com.au',
+  'The American Interest': 'the-american-interest.com',
+  'The Atlantic': 'theatlantic.com',
+  'The Australian': 'theaustralian.com.au',
+  'The Australian Financial Review': 'afr.com',
+  'The Boston Globe': 'bostonglobe.com',
+  'The Business Journals': 'bizjournals.com',
+  'The Canberra Times': 'canberratimes.com.au',
+  'The Economist': 'economist.com',
+  'The Globe and Mail': 'theglobeandmail.com',
+  'The Hindu': 'thehindu.com',
+  'The Irish Times': 'irishtimes.com',
+  'The Japan Times': 'japantimes.co.jp',
+  'TheMarker': 'themarker.com',
+  'The Mercury News': 'mercurynews.com',
+  'The Morning Call': 'mcall.com',
+  'The Nation': 'thenation.com',
+  'The News-Gazette': 'news-gazette.com',
+  'The New Statesman': 'newstatesman.com',
+  'The New York Times': 'nytimes.com',
+  'The New Yorker': 'newyorker.com',
+  'The Philadelphia Inquirer': 'inquirer.com',
+  'The Seattle Times': 'seattletimes.com',
+  'The Spectator': 'spectator.co.uk',
+  'The Sydney Morning Herald': 'smh.com.au',
+  'The Telegraph': 'telegraph.co.uk',
+  'The Times': 'thetimes.co.uk',
+  'The Toronto Star': 'thestar.com',
+  'The Washington Post': 'washingtonpost.com',
+  'The Wall Street Journal': 'wsj.com',
+  'Trouw': 'trouw.nl',
+  'Winston-Salem Journal': 'journalnow.com',
+  'Vanity Fair': 'vanityfair.com',
+  'Vrij Nederland': 'vn.nl',
+  'Wired': 'wired.com'
+};
+
+const restrictions = {
+  'barrons.com': 'barrons.com/articles'
+}
+
+// Don't remove cookies before page load
+const allow_cookies = [
+'ad.nl',
+'asia.nikkei.com',
+'bostonglobe.com',
+'canberratimes.com.au',
+'chicagobusiness.com',
+'demorgen.be',
+'denverpost.com',
+'economist.com',
+'ed.nl',
+'examiner.com.au',
+'ft.com',
+'harpers.org',
+'hbr.org',
+'lemonde.fr',
+'lesechos.fr',
+'medium.com',
+'mercurynews.com',
+'mexiconewsdaily.com',
+'nrc.nl',
+'nymag.com',
+'nytimes.com',
+'parool.nl',
+'qz.com',
+'scientificamerican.com',
+'seattletimes.com',
+'telegraaf.nl',
+'the-american-interest.com',
+'theadvocate.com.au',
+'theage.com.au',
+'theatlantic.com',
+'theaustralian.com.au',
+'trouw.nl',
+'vn.nl',
+'volkskrant.nl',
+'washingtonpost.com',
+'wsj.com'
+]
+
+// Removes cookies after page load
+const remove_cookies = [
+'ad.nl',
+'asia.nikkei.com',
+'bostonglobe.com',
+'canberratimes.com.au',
+'chicagobusiness.com',
+'demorgen.be',
+'denverpost.com',
+'economist.com',
+'ed.nl',
+'examiner.com.au',
+'ft.com',
+'harpers.org',
+'hbr.org',
+'lesechos.fr',
+'medium.com',
+'mercurynews.com',
+'mexiconewsdaily.com',
+'nrc.nl',
+'nymag.com',
+'qz.com',
+'scientificamerican.com',
+'seattletimes.com',
+'telegraaf.nl',
+'theadvocate.com.au',
+'theage.com.au',
+'theatlantic.com',
+'vn.nl',
+'washingtonpost.com',
+'wsj.com'
+]
+
+// select specific cookie(s) to hold from remove_cookies domains
+const remove_cookies_select_hold = {
+	'nrc.nl': ['nmt_closed_cookiebar'],
+	'washingtonpost.com': ['wp_gdpr'],
+	'wsj.com': ['wsjregion']
+}
+
+// select only specific cookie(s) to drop from remove_cookies domains
+const remove_cookies_select_drop = {
+	'ad.nl': ['temptationTrackingId'],
+	'demorgen.be': ['TID_ID'],
+	'ed.nl': ['temptationTrackingId'],
+	'nrc.nl': ['counter']
+}
+
+// Override User-Agent with Googlebot
+const use_google_bot = [
+'barrons.com',
+'lemonde.fr',
+'mexiconewsdaily.com',
+'nytimes.com',
+'quora.com',
+'telegraph.co.uk',
+'theaustralian.com.au',
+'thetimes.co.uk',
+'wsj.com',
+'zeit.de',
+]
+
+function setDefaultOptions() {
+  chrome.storage.sync.set({
+    sites: defaultSites
+  }, function() {
+    chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
+  });
+}
+
+var blockedRegexes = {
+'chicagotribune.com': /.+:\/\/.+\.tribdss\.com\//,
+'thenation.com': /thenation\.com\/.+\/paywall-script\.php/,
+'haaretz.co.il': /haaretz\.co\.il\/htz\/js\/inter\.js/,
+'nzherald.co.nz': /nzherald\.co\.nz\/.+\/headjs\/.+\.js/,
+'businessinsider.com': /(.+\.tinypass\.com\/.+|cdn\.onesignal\.com\/sdks\/.+\.js)/,
+'economist.com': /.+\.tinypass\.com\/.+/
+};
+
+const userAgentDesktop = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+const userAgentMobile = "Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible ; Googlebot/2.1 ; +http://www.google.com/bot.html)"
+
+var enabledSites = [];
+
+// Get the enabled sites
+chrome.storage.sync.get({
+  sites: {}
+}, function(items) {
+  var sites = items.sites;
+  enabledSites = Object.keys(items.sites).map(function(key) {
+    return items.sites[key];
+  });
+});
+
+// Listen for changes to options
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  var key;
+  for (key in changes) {
+    var storageChange = changes[key];
+    if (key === 'sites') {
+      var sites = storageChange.newValue;
+      enabledSites = Object.keys(sites).map(function(key) {
+        return sites[key];
+      });
+    }
+  }
+});
+
+// Set and show default options on install
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == "install") {
+    setDefaultOptions();
+  } else if (details.reason == "update") {
+    // User updated extension
+  }
+});
+
+/**
+// WSJ bypass
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+  if (!isSiteEnabled(details) || details.url.indexOf("mod=rsswn") !== -1) {
+    return;
+  }
+
+  var param;
+  var updatedUrl;
+
+  param = getParameterByName("mod", details.url);
+
+  if (param === null) {
+    updatedUrl = stripQueryStringAndHashFromPath(details.url);
+    updatedUrl += "?mod=rsswn";
+  } else {
+    updatedUrl = details.url.replace(param, "rsswn");
+  }
+  return { redirectUrl: updatedUrl};
+},
+{urls:["*://*.wsj.com/*"], types:["main_frame"]},
+["blocking"]
+);
+**/
+
+// Disable javascript for these sites
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  if (!isSiteEnabled(details) || details.url.indexOf("mod=rsswn") !== -1) {
+    return;
+  }
+  return {cancel: true}; 
+  },
+  {
+    urls: ["*://*.thestar.com/*", "*://*.theglobeandmail.com/*", "*://*.afr.com/*", "*://*.bostonglobe.com/*", "*://*.tinypass.com/*"],
+    types: ["script"]
+  },
+  ["blocking"]
+);
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+  var requestHeaders = details.requestHeaders;
+
+  var header_referer = '';
+  for (var n in requestHeaders) {
+	  if (requestHeaders[n].name.toLowerCase() == 'referer') {
+		  header_referer = requestHeaders[n].value;
+		  continue;
+	  }
+  }
+  
+  // check for blocked regular expression: domain enabled, match regex, block on an internal or external regex
+  for (var domain in blockedRegexes) {
+	  if (isSiteEnabled({url: '.'+ domain}) && details.url.match(blockedRegexes[domain])) {
+			if (details.url.indexOf(domain) !== -1 || header_referer.indexOf(domain) !== -1) {
+				return { cancel: true };
+			}
+	  }
+  }
+
+  if (!isSiteEnabled(details)) {
+    return;
+  }
+
+  var tabId = details.tabId;
+
+  var useUserAgentMobile = false;
+  var setReferer = false;
+
+  // if referer exists, set it to google
+  requestHeaders = requestHeaders.map(function (requestHeader) {
+    if (requestHeader.name === 'Referer') {
+      if (details.url.indexOf("wsj.com") !== -1 || details.url.indexOf("ft.com") !== -1) {
+        requestHeader.value = 'https://www.facebook.com/';
+      } else {
+        requestHeader.value = 'https://www.google.com/';
+      }
+      setReferer = true;
+    }
+    if (requestHeader.name === 'User-Agent') {
+      useUserAgentMobile = requestHeader.value.toLowerCase().includes("mobile");
+    }
+
+    return requestHeader;
+  });
+
+  // otherwise add it
+  if (!setReferer) {
+    if (details.url.indexOf("wsj.com") !== -1) {
+      requestHeaders.push({
+        name: 'Referer',
+        value: 'https://www.facebook.com/'
+      });
+    } else {
+      requestHeaders.push({
+        name: 'Referer',
+        value: 'https://www.google.com/'
+      });
+    }
+
+  }
+
+  // override User-Agent to use Googlebot
+  var useGoogleBot = use_google_bot.filter(function(item) {
+    return typeof item == 'string' && details.url.indexOf(item) > -1;            
+  }).length > 0;
+
+  if (useGoogleBot) {
+    requestHeaders.push({
+      "name": "User-Agent",
+      "value": useUserAgentMobile ? userAgentMobile : userAgentDesktop
+    })
+    requestHeaders.push({
+      "name": "X-Forwarded-For",
+      "value": "66.249.66.1"
+    })
+  }
+
+  // remove cookies before page load
+  requestHeaders = requestHeaders.map(function(requestHeader) {
+    for (var siteIndex in allow_cookies) {
+      if (details.url.indexOf(allow_cookies[siteIndex]) !== -1) {
+        return requestHeader;
+      }
+    }
+    if (requestHeader.name === 'Cookie') {
+      requestHeader.value = '';
+    }
+    return requestHeader;
+  });
+
+  if (tabId !== -1) {
+    // run contentScript inside tab
+    chrome.tabs.executeScript(tabId, {
+      file: 'contentScript.js',
+      runAt: 'document_start'
+    }, function(res) {
+      if (chrome.runtime.lastError || res[0]) {
+        return;
+      }
+    });
+  }
+
+  return { requestHeaders: requestHeaders };
+}, {
+  urls: ['<all_urls>']
+}, ['blocking', 'requestHeaders', 'extraHeaders']);
+
+// remove cookies after page load
+chrome.webRequest.onCompleted.addListener(function(details) {
+  for (var domainIndex in remove_cookies) {
+    var domainVar = remove_cookies[domainIndex];
+    if (!enabledSites.includes(domainVar) || details.url.indexOf(domainVar) === -1) {
+      continue; // don't remove cookies
+    }
+    chrome.cookies.getAll({domain: domainVar}, function(cookies) {
+		for (var i=0; i<cookies.length; i++) {
+			var cookie_domain = cookies[i].domain;
+			var rc_domain = cookie_domain.replace(/^(\.?www\.|\.)/, '');
+			// hold specific cookie(s) from remove_cookies domains
+			if ((rc_domain in remove_cookies_select_hold) && remove_cookies_select_hold[rc_domain].includes(cookies[i].name)) {
+				continue; // don't remove specific cookie
+			}
+			// drop only specific cookie(s) from remove_cookies domains
+			if ((rc_domain in remove_cookies_select_drop) && !(remove_cookies_select_drop[rc_domain].includes(cookies[i].name))) {
+				continue; // only remove specific cookie
+			}
+			chrome.cookies.remove({url: (cookies[i].secure ? "https://" : "http://") + cookies[i].domain + cookies[i].path, name: cookies[i].name});
+		}
+    });
+  }
+}, {
+  urls: ["<all_urls>"]
+});
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-69824169-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function isSiteEnabled(details) {
+  var isEnabled = enabledSites.some(function(enabledSite) {
+    var useSite = details.url.indexOf("." + enabledSite) !== -1;
+    if (enabledSite in restrictions) {
+      return useSite && details.url.indexOf(restrictions[enabledSite]) !== -1;
+    }
+    return useSite;
+  });
+  return isEnabled;
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function stripQueryStringAndHashFromPath(url) {
+  return url.split("?")[0].split("#")[0];
+}
